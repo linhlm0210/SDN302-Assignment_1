db5a49f533e23ce6adeb4ce9209e1ba62dc96788
linhlm <lammylinh7012@gmail.com>
2024-09-27 20:40:25 +0700

    remove unused code

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
-import express from "express"
-import mongoose from "mongoose"
-import bodyParser from "body-parser"
-import dotenv from "dotenv"
-import quizzesRoute from "./routes/quizzesRoute.js"
-import questionRoute from "./routes/questionRoute.js"
-import { engine } from 'express-handlebars';
-import cors from 'cors'
+/* eslint-disable no-undef */
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import quizzesRoute from "./routes/quizzesRoute.js";
+import questionRoute from "./routes/questionRoute.js";
+import { engine } from "express-handlebars";
 
-const app = express ();
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.engine('hbs', engine({extname: 'hbs'}));
-app.set('view engine', 'hbs');
-app.set('views', './resources/views')
+app.engine("hbs", engine({ extname: "hbs" }));
+app.set("view engine", "hbs");
+app.set("views", "./resources/views");
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-const MONGOURL = process.env.MONGO_URL; 
+const MONGOURL = process.env.MONGO_URL;
 
-mongoose.connect(MONGOURL).then(()=>{
-    console.log("Database connected Successfully.")
-    app.listen(PORT, ()=>{
-        console.log(`Server is running on port : ${PORT}`)
-    })
-}).catch(error => console.log(error));
-
-let corsOptions = {
-    origin : ['http://localhost:3001'],
- }
- 
- app.use(cors(corsOptions))
- 
+mongoose
+  .connect(MONGOURL)
+  .then(() => {
+    console.log("Database connected Successfully.");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port : ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(error));
 
 app.use("/quizzes", quizzesRoute);
 app.use("/question", questionRoute);
